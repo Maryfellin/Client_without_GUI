d0b73c735d99dbe7632776b0f7fe70ad17ba4382
Maryfellin <maryfelin@gmail.com>
2017-04-20 23:51:46 +0300

    20.04.2017
    Реализован обмен сообщениями между клиентами.

--- ClientMasha/client1.cpp
+++ ClientMasha/client1.cpp
@@ -3,6 +3,10 @@
 #include <string> 
 #include <iostream> 
 
+#include <windows.h>
+
+using namespace std;
+
 SOCKET Connection;
 
 char ServerIP[] = "127.0.0.1";
@@ -11,26 +15,33 @@ std::string nickname;
 
 enum Packet
 {
-	P_ChatMessage,
-	P_Test
+	Message,
+	Test
 };
+
+BOOL WINAPI SetConsoleOutputCP(__in UINT wCodePageID); //установка кодовой страницы в поток вывода
+BOOL WINAPI SetConsoleCP(__in UINT wCodePageID); //установка кодовой страницы в поток вывода
+
 
 bool ProcessPacket(Packet packettype)
 {
 	switch (packettype)
 	{
-	case P_ChatMessage:
+	case Message:
 	{
 		int bufferlength; //длина сообщения
 		recv(Connection, (char*)&bufferlength, sizeof(int), NULL); //Буфер приема
 		char * buffer = new char[bufferlength + 1]; //Выделяем буфер
+
 		buffer[bufferlength] = '\0';
 		recv(Connection, buffer, bufferlength, NULL);
 		printf(buffer); //печатаем
+		printf("\n");
+
 		delete[] buffer; //удаляем буфер
 		break;
 	}
-	case P_Test:
+	case Test:
 		printf("Тестовое сообщение отправлено!\n");
 		break;
 	default:
@@ -47,6 +58,8 @@ void ClientThread()
 	{
 		//Сначала получаем тип пакета
 		recv(Connection, (char*)&packettype, sizeof(Packet), NULL); //Получение типа пакета с сервера
+		printf("\n");
+
 
 		if (!ProcessPacket(packettype)) //Если пакет неправильно обработан
 			break; //выход из цикла
@@ -55,17 +68,19 @@ void ClientThread()
 
 int main()
 {
+	//setlocale(LC_ALL, "russian");
 
-	setlocale(LC_ALL, "russian");
+	SetConsoleCP(1251); //установка кодовой страницы win-cp 1251 в поток ввода
+	SetConsoleOutputCP(1251); //установка кодовой страницы win-cp 1251 в поток вывода
+
 	WSADATA WSAData;
 	WSAStartup(MAKEWORD(2, 0), &WSAData);
 
-	SOCKADDR_IN addr; 
+	SOCKADDR_IN addr;
 	int sizeofaddr = sizeof(addr);
 	addr.sin_addr.s_addr = inet_addr(ServerIP);
 	addr.sin_port = htons(Port);
 	addr.sin_family = AF_INET;
-
 
 
 	std::cout << "Введите имя: ";
@@ -74,27 +89,31 @@ int main()
 	std::cout << "Приветствую, " << nickname << "!!! \n";
 
 	Connection = socket(AF_INET, SOCK_STREAM, NULL);
-	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) 
+	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0)
 	{
 		MessageBoxA(NULL, "Ошибка подключения", "Error", MB_OK | MB_ICONERROR);
-		return 0; 
+		return 0;
 
 	}
-	else printf ("Подключён к серверу!\n");
+	else printf("Подключён к серверу!\n");
 
 
-	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL); 
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL);
 
 
+	std::string name;
 	std::string userinput; //содержит сообщение чата пользователя
+
 	while (true)
 	{
 		std::getline(std::cin, userinput); //Получить строку, если пользователь нажимает клавишу enter и заполняет буфер
-		int bufferlength = userinput.size(); //Найти длину буфера
-		Packet chatmessagepacket = P_ChatMessage; //Создать тип пакета: сообщение чата, отправляемое на сервер
+		name = nickname + ": " + userinput;
+		int bufferlength = name.size(); //Найти длину буфера
+		Packet chatmessagepacket = Message; //Создать тип пакета: сообщение чата, отправляемое на сервер
+
 		send(Connection, (char*)&chatmessagepacket, sizeof(Packet), NULL); //Отправить тип пакета: сообщение чата
 		send(Connection, (char*)&bufferlength, sizeof(int), NULL); //Отправить длину буфера
-		send(Connection, userinput.c_str(), bufferlength, NULL); //Отправить буфер
+		send(Connection, name.c_str(), bufferlength, NULL); //Отправить буфер 
 		Sleep(10);
 	}
 	return 0;
